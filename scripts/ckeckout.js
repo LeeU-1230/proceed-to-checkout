f23e59045c93e7fdb13af0366a7958df5c61d0bd
LeeU1230 <name10831@gmail.com>
2020-10-23 17:42:21 +0800

    更改change觸發計算

--- scripts/ckeckout.js
+++ scripts/ckeckout.js
@@ -9,17 +9,20 @@ let total = prod01 + prod02 + 80;
 let pickupway;
 
 prod_qty01.addEventListener('change', function () {
-    prod01 = prod_qty01.value * prod01;
+    prod01 = prod_qty01.value * 150;
     prod_subtotal01.textContent = `NT$ ${prod01}`;
     total_price.textContent = `NT$ ${prod01 + prod02 + 80}`;
     total = prod01 + prod02 + 80;
+    console.log(prod01);
+
 })
 
 prod_qty02.addEventListener('change', function () {
-    prod02 = prod_qty02.value * prod02;
+    prod02 = prod_qty02.value * 160;
     prod_subtotal02.textContent = `NT$ ${prod02}`;
     total_price.textContent = `NT$ ${prod01 + prod02 + 80}`;
     total = prod01 + prod02 + 80;
+    console.log(prod02);
 })
 
 /*function all_total(){
@@ -32,18 +35,18 @@ let a_COD = document.querySelector('.a_COD');
 let a_ATM = document.querySelector('.a_ATM');
 let a_IBO = document.querySelector('.a_IBO');
 
-a_creditcard.addEventListener('click',function(){
-    a_creditcard.setAttribute('href',`htmls/creditcard.html?${total}`);
+a_creditcard.addEventListener('click', function () {
+    a_creditcard.setAttribute('href', `htmls/creditcard.html?${total}`);
 })
 
-a_COD.addEventListener('click',function(){
-    a_COD.setAttribute('href',`htmls/COD.html?${total}`);
+a_COD.addEventListener('click', function () {
+    a_COD.setAttribute('href', `htmls/COD.html?${total}`);
 })
 
-a_ATM.addEventListener('click',function(){
-    a_ATM.setAttribute('href',`htmls/ATM.html?${total}`);
+a_ATM.addEventListener('click', function () {
+    a_ATM.setAttribute('href', `htmls/ATM.html?${total}`);
 })
 
-a_IBO.addEventListener('click',function(){
-    a_IBO.setAttribute('href',`htmls/ibon.html?${total}`);
+a_IBO.addEventListener('click', function () {
+    a_IBO.setAttribute('href', `htmls/ibon.html?${total}`);
 })
